dbaacef749f7cd58c8006e925a7ee4eba466b734
Trần Nguyên Bình <tnguyenbinhxyz@gmail.com>
2024-05-14 18:40:25 +0700

    Update Banana item

--- Assets/WepConfig.cs
+++ Assets/WepConfig.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class WepConfig : MonoBehaviour
 {
     [SerializeField] int typeWep;
-    [SerializeField] int LevelRN;
-    [SerializeField] int oldLevel;
+    [SerializeField] int LevelRN0;
+    [SerializeField] int LevelRN1;
+    [SerializeField] int oldLevel0;
+    [SerializeField] int oldLevel1;
     [SerializeField] GameObject[] LevelWep;
     [SerializeField] GameObject[] fixedLevelWep;
     [SerializeField] public float bulletSpeed;
     void Start()
     {
+         if (typeWep == 0) 
+        { 
         LevelWep[0].SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -24,16 +29,34 @@ public class WepConfig : MonoBehaviour
     void checkLevel()
     {
         
-        LevelRN = GameObject.Find("Character").GetComponent<PlayerInfo>().PlayerLevel[typeWep];
+        
         if (typeWep == 0) 
         { 
-        if (LevelRN != oldLevel)
+            LevelRN0 = GameObject.Find("Character").GetComponent<PlayerInfo>().PlayerLevel[typeWep];
+        if (LevelRN0 != oldLevel0)
         {
-            if (LevelRN == 2) { LevelWep[1].SetActive(true); LevelWep[0].GetComponent<Shooting>().shootTimer = 0f; }
-            if (LevelRN == 3) { LevelWep[2].SetActive(true); LevelWep[0].GetComponent<Shooting>().shootTimer = 0f; fixedLevelWep[0].GetComponent<Shooting>().shootTimer = 0f; fixedLevelWep[1].GetComponent<Shooting>().shootTimer = 0f; }
-            oldLevel = LevelRN;
+            if (LevelRN0 == 2) { LevelWep[1].SetActive(true); LevelWep[0].GetComponent<Shooting>().shootTimer = 0f; }
+            if (LevelRN0 == 3) { LevelWep[2].SetActive(true); LevelWep[0].GetComponent<Shooting>().shootTimer = 0f; fixedLevelWep[0].GetComponent<Shooting>().shootTimer = 0f; fixedLevelWep[1].GetComponent<Shooting>().shootTimer = 0f; }
+            oldLevel0 = LevelRN0;
         }
         }
+        if (typeWep ==1) 
+        { 
+            LevelRN1 = GameObject.Find("Character").GetComponent<PlayerInfo>().PlayerLevel[typeWep];
+            if (LevelRN1 != oldLevel1)
+        {
+            if (LevelRN1 == 1) { Debug.Log("Da active 1 "); LevelWep[0].SetActive(true); LevelWep[0].GetComponent<Shooting>().shootTimer = 0f; LevelWep[1].SetActive(true); LevelWep[1].GetComponent<Shooting>().shootTimer = 0f; }
+            
+            if (LevelRN1 == 2) { LevelWep[2].SetActive(true); LevelWep[2].GetComponent<Shooting>().shootTimer = 0f; LevelWep[3].SetActive(true); LevelWep[3].GetComponent<Shooting>().shootTimer = 0f; }
+            Debug.Log("Da active 2 ");
+            
+            Debug.Log("Da active 3 ");
+            if (LevelRN1 == 3) { LevelWep[4].SetActive(true); LevelWep[4].GetComponent<Shooting>().shootTimer = 0f; LevelWep[5].SetActive(true); LevelWep[5].GetComponent<Shooting>().shootTimer = 0f; }
+            oldLevel1 = LevelRN1;
+        }
+            
+        }
+
 
     }
 }
